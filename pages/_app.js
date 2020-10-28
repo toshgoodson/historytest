6cc4126a49168718b5b9c1d0091b2750ec37a8f8
Tosh Goodson <tosh.goodson@gmail.com>
2020-10-28 14:01:37 -0700

    history test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '../styles/globals.css'
 
+
+try {
+  // inspect pop state
+  window.addEventListener('popstate', (event) => {
+    console.log('pop', event)
+  })
+  // inspect replaceState
+  let _replaceState = history.replaceState
+  history.replaceState = function() { console.log("replace", arguments); _replaceState.apply(history, arguments) }
+  // inspect pushState
+  let _pushState= history.pushState
+  history.pushState = function() { console.log("push", arguments); _pushState.apply(history, arguments) }
+} catch (err) {}
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
---
+++ pages/second-page.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function SecondPage() {
+  return (
+    <div className={styles.container}>
+      Second Page
+    </div>
+  )
+}
